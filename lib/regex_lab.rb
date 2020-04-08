00c0e07464aef5a965c3c6583365dc19159572f2
Thomas John Helin <thomashelin@yahoo.com>
2020-04-08 15:56:57 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
- if text.match(/^A-Z\.$/)
+ if text.match(/^A-Z\w*\.$/)
    return true
  else
    return false
