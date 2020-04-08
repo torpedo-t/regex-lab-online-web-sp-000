e303f8134b71e8434ecf42da199fd8e5b890c5e2
Thomas John Helin <thomashelin@yahoo.com>
2020-04-08 15:43:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,6 @@
 def starts_with_a_vowel?(word)
- if word.match(/^aeiou/i)
-   #{}/^[aeiou]/i.match(word)
+ if word.match(/^[aeiou]/i)
+  # /^[aeiou]/i.match(word)
    return true
  else
    return false
