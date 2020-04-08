34b9e1ab4f3f1de6cb455e790207cb45f2d811d4
Thomas John Helin <thomashelin@yahoo.com>
2020-04-08 15:33:48 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,7 +6,7 @@ def starts_with_a_vowel?(word)
 end
 end
 def words_starting_with_un_and_ending_with_ing(text)
- text.scan(/^un\D*ing/)
+ text.scan(/\bun\D*ing/)
 end
 def words_five_letters_long(text)
 
