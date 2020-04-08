2d8d50917279eb0e6bd4e8d0c28b9784b8bc020c
Thomas John Helin <thomashelin@yahoo.com>
2020-04-08 16:18:44 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,7 +20,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 end
 def valid_phone_number?(phone)
- if phone.match(/\A.[1-9]{3}.[1-9]{3}.[1-9]{4}\z/)
+ if phone.match(/^.[1-9]{3}.[1-9]{3}.[1-9]{4}\z/)
    return true
  else
    return false
