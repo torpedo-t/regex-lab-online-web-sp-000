fa51787516810c511e7e3928082f07e3c4d8521e
Thomas John Helin <thomashelin@yahoo.com>
2020-04-08 16:38:54 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,7 +20,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 end
 def valid_phone_number?(phone)
- if phone.match(/([0-9] *){10}|(\([0-9]{3}\)(
+ if phone.match(/^.*[0-9]{3,}.*[1-9]{3,}.*[1-9]{4}$/)
    return true
  else
    return false
