e1fd65fada261bc1d3b40e56e3ec4eb82c007ed2
Thomas John Helin <thomashelin@yahoo.com>
2020-04-08 16:41:32 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
 def starts_with_a_vowel?(word)
-
+ if word.match(/^[aeiou]/i)
+   return true
+ else
+   return false
 end
-
+end
 def words_starting_with_un_and_ending_with_ing(text)
-
+ text.scan(/\bun\w*ing\b/)
 end
-
 def words_five_letters_long(text)
-
+ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+ if text.match(/^[A-Z].*\.$/)
+   return true
+ else
+   return false
 end
-
+end
 def valid_phone_number?(phone)
-
+ if phone.match(/^.*[0-9]{3,}.*[1-9]{3,}.*[1-9]{4}$/)
+   return true
+ else
+   return false
+ end
 end
