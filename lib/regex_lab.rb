2fce983c0515ae4e37eca7cf1175acf4e7181ba1
Thomas John Helin <thomashelin@yahoo.com>
2020-04-08 15:11:06 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,11 +1,13 @@
 def starts_with_a_vowel?(word)
-
+ if /^[aeiou]/i.match(word)
+   return true
+ else
+   return false
 end
-
+end
 def words_starting_with_un_and_ending_with_ing(text)
-
+ text.scan(/^un\D*ing/)
 end
-
 def words_five_letters_long(text)
 
 end
